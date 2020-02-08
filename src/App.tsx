72de9c40bec478bbe981e559e67053ddf392636d
Gerald Maboshe <gerald@thezig.io>
2020-02-09 00:31:35 +0200

    implemented counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,32 @@
-import React, { Component} from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component{
-  render(){
-    return(
-        <div>
-            <button>Add</button>
-        </div>
-  );
+interface IProps {
+
+}
+interface IState {
+  count: any
+}
+
+class App extends Component<IProps, IState> {
+  constructor(props: IProps){
+    super(props);
+    this.state = {
+      count: 0
+    }
+  }
+  
+  render() {
+    const handleButtonClick = () => {
+      this.setState({ count: this.state.count + 1})
+    }
+    return (
+      <div>
+        <h1>{ this.state.count}</h1>
+        <button onClick = { handleButtonClick}>Add</button>
+      </div>
+    );
   }
 }
 export default App;
